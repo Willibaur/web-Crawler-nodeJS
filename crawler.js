2fa9cabfe24a42b5feda90bd231382f9dce69265
William Bautista <williambautista@gmail.com>
2016-05-09 22:18:04 +0100

    Visit URL and response functions added

--- crawler.js
+++ crawler.js
@@ -1,3 +1,20 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL     = require('url-parse');
+
+var pageToVisit = "https://gocardless.com";
+
+console.log("Visiting page " + pageToVisit);
+
+request(pageToVisit, function (error, response, body) {
+  if (error) {
+    console.log("Error: " + error);
+  }
+
+  console.log("Status code: " + response.statusCode);
+
+  if(response.status === 200) {
+    var $ = cheerio.load(body);
+    console.log("Page title: " + $('title').text());
+  }
+});
