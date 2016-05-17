3443d0f6770e6cad32000b493fca1829977ef880
William Bautista <williambautista@gmail.com>
2016-05-17 16:26:46 +0100

    Initial URL accepted to crawl

--- crawler.js
+++ crawler.js
@@ -2,19 +2,60 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL     = require('url-parse');
 
-var pageToVisit = "https://gocardless.com";
 
-console.log("Visiting page " + pageToVisit);
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " URL");
+    process.exit(-1);
+}
 
-request(pageToVisit, function (error, response, body) {
-  if (error) {
-    console.log("Error: " + error);
+var START_URL           = process.argv[2];
+var MAX_PAGES_TO_VISIT  = 100;
+
+var pagesVisited    = {};
+var numPagesVisited = 0;
+var pagesToVisit    = [];
+
+var url     = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    console.log(pagesVisited);
+    return;
   }
-
-  console.log("Status code: " + response.statusCode);
-
-  if(response.status === 200) {
-    var $ = cheerio.load(body);
-    console.log("Page title: " + $('title').text());
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    crawl();
+  } else {
+    visitPage(nextPage, crawl);
   }
-});
+}
+
+function visitPage(url, callback) {
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+
+     // Parse the document body
+     var $ = cheerio.load(body);
+       collectInternalLinks($);
+       callback();
+  });
+}
+
+function collectInternalLinks($) {
+    var relativeLinks = $("a[href^='/']");
+    relativeLinks.each(function() {
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
+    });
+}
